d970ec3b14af1d77003d5dbe4ee5a0a489d834b5
cliffmieser <cliffmieser@gmail.com>
2023-02-16 17:46:55 -0500

    css & js scripts added

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+let box = document.getElementById('div-box'); //select the div tag
+let radius = document.querySelector('#radius'); //select the range input
+let output = document.querySelector('.radius-output');
+let borderRadVals = [ 0, 0, 0, 0];
+
+const [a, b, c, d] = borderRadVals;
+console.log(borderRadVals);
+
+output.textContent = radius.value;
+
+radius.addEventListener('input', () => {
+    output.textContent = radius.value;
+    setBorderRadius(borderRadVals);
+    console.log(borderRadVals);
+    box.style.borderRadius = `${a}% ${b}% ${c}% ${d}%`;
+})
+
+
+
+
+let setBorderRadius = (arr) => {
+    for(let val in arr){
+        val = radius.value;
+    }
+
+}
